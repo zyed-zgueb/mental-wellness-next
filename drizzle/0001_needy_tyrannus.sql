8596e906907ea944a52fbec7287d8f29824e70f3
Zyed <zyed.zgueb@gmail.com>
2025-12-10 16:08:38 +0100

    first commit

---
+++ drizzle/0001_needy_tyrannus.sql
@@ -0,0 +1,5 @@
+CREATE INDEX "account_user_id_idx" ON "account" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "account_provider_account_idx" ON "account" USING btree ("provider_id","account_id");--> statement-breakpoint
+CREATE INDEX "session_user_id_idx" ON "session" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX "session_token_idx" ON "session" USING btree ("token");--> statement-breakpoint
+CREATE INDEX "user_email_idx" ON "user" USING btree ("email");
